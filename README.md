f4202b1cc90ee514da888904a8d31f8933f3bcf9
williandiniz <williandiniz@gmail.com>
2022-07-12 11:04:31 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,93 @@
-# sentry
+# Sentry Nightly On-Premise Docker Image
+
+This is a customized docker image including 3rd Party Plugins that can be used by OpenShift.
+
+## Provisioning
+
+Import `sentry-template.yaml` into your OpenShift Project and process the template.
+
+A `BuildConfig`, `ImageStream`, `ConfigMap` and `Secret` is created, which can be used for the further setup of the on-premise.
+
+If you want to customize the Sentry Docker image and the other OpenShift objects, clone the repo and make the respective changes inside the template. Add or remove plugins you need.
+
+### Environment Variables
+
+**`SENTRY_SECRET_KEY`**
+
+A secret key used for cryptographic functions within Sentry. This key should be unique and consistent across all running instances. You can generate a new secret key doing something like:
+
+**`SENTRY_POSTGRES_HOST`, `SENTRY_POSTGRES_PORT`, `SENTRY_DB_NAME`, `SENTRY_DB_USER`, `SENTRY_DB_PASSWORD`**
+
+Database credentials for your Postgres server. These values aren't needed if a linked postgres container exists.
+
+**`SENTRY_SERVER_EMAIL`**
+
+The email address used for `From:` in outbound emails. Default: `root@localhost`
+
+**`SENTRY_EMAIL_HOST`, `SENTRY_EMAIL_PORT`, `SENTRY_EMAIL_USER`, `SENTRY_EMAIL_PASSWORD`, `SENTRY_EMAIL_USE_TLS`**
+
+Connection information for an outbound smtp server. These values aren't needed if a linked `smtp` container exists.
+
+**`SENTRY_MAILGUN_API_KEY`**
+
+If you're using Mailgun for inbound mail, set your API key and configure a route to forward to `/api/hooks/mailgun/inbound/`.
+
+More Information:
+
+- https://hub.docker.com/_/sentry/
+
+## Plugins
+
+There are several interfaces currently available to extend Sentry. These are a work in progress and the API is not frozen.
+
+More Information: https://docs.sentry.io/server/plugins/
+
+### 3rd Party Plugins
+
+#### sentry-auth-oidc
+
+> An SSO provider for Sentry which enables OpenID Connect Apps authentication.
+
+Add `https://sentry.example.com/auth/sso/` to the authorized redirect URIs (on your IdP).
+
+Set the following environment variables for your oidc integration inside of the created `Secret`:
+
+```yaml
+# Example
+OIDC_CLIENT_ID: ""
+OIDC_CLIENT_SECRET: ""
+OIDC_SCOPE: "openid email"
+OIDC_DOMAIN: "https://accounts.google.com"
+```
+
+The OIDC_DOMAIN defines where the OIDC configuration is going to be pulled from. Basically it specifies the OIDC server and adds the path `.well-known/openid-configuration` to it. That's where different endpoint paths can be found.
+
+You can also define `OIDC_ISSUER` to change the default provider name in the UI, even when the `OIDC_DOMAIN` is set.
+
+If your provider doesn't support the `OIDC_DOMAIN`, then you have to set these required endpoints by yourself (autorization_endpoint, token_endpoint, userinfo_endpoint, issuer).
+
+```yaml
+# Example
+OIDC_AUTHORIZATION_ENDPOINT: "https://accounts.google.com/o/oauth2/v2/auth"
+OIDC_TOKEN_ENDPOINT: "https://www.googleapis.com/oauth2/v4/token"
+OIDC_USERINFO_ENDPOINT: "[openid email](https://www.googleapis.com/oauth2/v3/userinfo)"
+OIDC_ISSUER: "Google"
+```
+
+#### sentry-msteams
+
+> Microsoft Teams Integration for Sentry Error Tracking Software.
+
+Go to [https://<SENTRY_URL>/settings/<ORGANIZATION_NAME>/projects/<PROJECT_NAME>/plugins/](https://<SENTRY_URL>/settings/<ORGANIZATION_NAME>/projects/<PROJECT_NAME>/plugins/) to enable and configure the Microsoft Teams plugin
+
+More Information: https://github.com/Neko-Design/sentry-msteams
+
+## Deprovisioning
+
+```bash
+oc delete all -l app=<name>
+oc delete configmap <configmap-name>
+oc delete secret <secret-name>
+# ATTTENTION! The following command is only optional and will permanently delete all of your data.
+oc delete pvc -l app=<name>
+```
--- sentry.conf.py
+++ sentry.conf.py
@@ -24,27 +24,45 @@
 #  SENTRY_EMAIL_USER
 #  SENTRY_EMAIL_PASSWORD
 #  SENTRY_EMAIL_USE_TLS
-#  SENTRY_EMAIL_LIST_NAMESPACE
 #  SENTRY_ENABLE_EMAIL_REPLIES
 #  SENTRY_SMTP_HOSTNAME
 #  SENTRY_MAILGUN_API_KEY
 #  SENTRY_SINGLE_ORGANIZATION
 #  SENTRY_SECRET_KEY
-#  SLACK_CLIENT_ID
-#  SLACK_CLIENT_SECRET
-#  SLACK_VERIFICATION_TOKEN
-#  GITHUB_APP_ID
-#  GITHUB_API_SECRET
-#  BITBUCKET_CONSUMER_KEY
-#  BITBUCKET_CONSUMER_SECRET
+
 from sentry.conf.server import *  # NOQA
+from sentry.utils.types import Bool
 
-import os
-import os.path
 
-CONF_ROOT = os.path.dirname(__file__)
+# Generously adapted from pynetlinux: https://git.io/JJmga
+def get_internal_network():
+    import ctypes
+    import fcntl
+    import math
+    import socket
+    import struct
 
-postgres = env('SENTRY_POSTGRES_HOST') or (env('POSTGRES_PORT_5432_TCP_ADDR') and 'postgres')
+    iface = b"eth0"
+    sockfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ifreq = struct.pack(b"16sH14s", iface, socket.AF_INET, b"\x00" * 14)
+
+    try:
+        ip = struct.unpack(
+            b"!I", struct.unpack(b"16sH2x4s8x", fcntl.ioctl(sockfd, 0x8915, ifreq))[2]
+        )[0]
+        netmask = socket.ntohl(
+            struct.unpack(b"16sH2xI8x", fcntl.ioctl(sockfd, 0x891B, ifreq))[2]
+        )
+    except IOError:
+        return ()
+    base = socket.inet_ntoa(struct.pack(b"!I", ip & netmask))
+    netmask_bits = 32 - int(round(math.log(ctypes.c_uint32(~netmask).value + 1, 2), 1))
+    return "{0:s}/{1:d}".format(base, netmask_bits)
+
+INTERNAL_SYSTEM_IPS = (get_internal_network(),)
+
+postgres = env('SENTRY_POSTGRES_HOST') or (
+    env('POSTGRES_PORT_5432_TCP_ADDR') and 'postgres')
 if postgres:
     DATABASES = {
         'default': {
@@ -69,11 +87,9 @@ if postgres:
                 env('SENTRY_POSTGRES_PORT')
                 or ''
             ),
-            'OPTIONS': {
-                'autocommit': True,
-            },
         },
     }
+
 
 # You should not change this setting after your database has been created
 # unless you have altered all schemas first
@@ -88,7 +104,10 @@ SENTRY_USE_BIG_INTS = True
 
 # Instruct Sentry that this install intends to be run by a single organization
 # and thus various UI optimizations should be enabled.
-SENTRY_SINGLE_ORGANIZATION = env('SENTRY_SINGLE_ORGANIZATION', True)
+SENTRY_SINGLE_ORGANIZATION = Bool(env('SENTRY_SINGLE_ORGANIZATION', True))
+
+SENTRY_OPTIONS["system.event-retention-days"] = int(env(
+    'SENTRY_EVENT_RETENTION_DAYS', "90"))
 
 #########
 # Redis #
@@ -97,52 +116,11 @@ SENTRY_SINGLE_ORGANIZATION = env('SENTRY_SINGLE_ORGANIZATION', True)
 # Generic Redis configuration used as defaults for various things including:
 # Buffers, Quotas, TSDB
 
-redis = env('SENTRY_REDIS_HOST') or (env('REDIS_PORT_6379_TCP_ADDR') and 'redis')
-if not redis:
-    raise Exception('Error: REDIS_PORT_6379_TCP_ADDR (or SENTRY_REDIS_HOST) is undefined, did you forget to `--link` a redis container?')
-
-redis_password = env('SENTRY_REDIS_PASSWORD') or ''
-redis_port = env('SENTRY_REDIS_PORT') or '6379'
-redis_db = env('SENTRY_REDIS_DB') or '0'
-
-SENTRY_OPTIONS.update({
-    'redis.clusters': {
-        'default': {
-            'hosts': {
-                0: {
-                    'host': redis,
-                    'password': redis_password,
-                    'port': redis_port,
-                    'db': redis_db,
-                },
-            },
-        },
-    },
-})
-
-#########
-# Cache #
-#########
-
-# Sentry currently utilizes two separate mechanisms. While CACHES is not a
-# requirement, it will optimize several high throughput patterns.
-
-memcached = env('SENTRY_MEMCACHED_HOST') or (env('MEMCACHED_PORT_11211_TCP_ADDR') and 'memcached')
-if memcached:
-    memcached_port = (
-        env('SENTRY_MEMCACHED_PORT')
-        or '11211'
-    )
-    CACHES = {
-        'default': {
-            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-            'LOCATION': [memcached + ':' + memcached_port],
-            'TIMEOUT': 3600,
-        }
+SENTRY_OPTIONS["redis.clusters"] = {
+    "default": {
+        "hosts": {0: {"host": "redis", "password": "", "port": "6379", "db": "0"}}
     }
-
-# A primary cache is required for things such as processing events
-SENTRY_CACHE = 'sentry.cache.redis.RedisCache'
+}
 
 #########
 # Queue #
@@ -152,27 +130,45 @@ SENTRY_CACHE = 'sentry.cache.redis.RedisCache'
 # information on configuring your queue broker and workers. Sentry relies
 # on a Python framework called Celery to manage queues.
 
-rabbitmq = env('SENTRY_RABBITMQ_HOST') or (env('RABBITMQ_PORT_5672_TCP_ADDR') and 'rabbitmq')
-
-if rabbitmq:
-    BROKER_URL = (
-        'amqp://' + (
-            env('SENTRY_RABBITMQ_USERNAME')
-            or env('RABBITMQ_ENV_RABBITMQ_DEFAULT_USER')
-            or 'guest'
-        ) + ':' + (
-            env('SENTRY_RABBITMQ_PASSWORD')
-            or env('RABBITMQ_ENV_RABBITMQ_DEFAULT_PASS')
-            or 'guest'
-        ) + '@' + rabbitmq + '/' + (
-            env('SENTRY_RABBITMQ_VHOST')
-            or env('RABBITMQ_ENV_RABBITMQ_DEFAULT_VHOST')
-            or '/'
-        )
+rabbitmq_host = None
+if rabbitmq_host:
+    BROKER_URL = "amqp://{username}:{password}@{host}/{vhost}".format(
+        username="guest", password="guest", host=rabbitmq_host, vhost="/"
     )
 else:
-    BROKER_URL = 'redis://:' + redis_password + '@' + redis + ':' + redis_port + '/' + redis_db
+    BROKER_URL = "redis://:{password}@{host}:{port}/{db}".format(
+        **SENTRY_OPTIONS["redis.clusters"]["default"]["hosts"][0]
+    )
 
+
+#########
+# Cache #
+#########
+
+# Sentry currently utilizes two separate mechanisms. While CACHES is not a
+# requirement, it will optimize several high throughput patterns.
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+        "LOCATION": ["memcached:11211"],
+        "TIMEOUT": 3600,
+    }
+}
+
+# A primary cache is required for things such as processing events
+SENTRY_CACHE = "sentry.cache.redis.RedisCache"
+
+DEFAULT_KAFKA_OPTIONS = {
+    "bootstrap.servers": "kafka-service:9092",
+    "message.max.bytes": 50000000,
+    "socket.timeout.ms": 1000,
+}
+
+SENTRY_EVENTSTREAM = "sentry.eventstream.kafka.KafkaEventStream"
+SENTRY_EVENTSTREAM_OPTIONS = {"producer_configuration": DEFAULT_KAFKA_OPTIONS}
+
+KAFKA_CLUSTERS["default"] = DEFAULT_KAFKA_OPTIONS
 
 ###############
 # Rate Limits #
@@ -181,7 +177,7 @@ else:
 # Rate limits apply to notification handlers and are enforced per-project
 # automatically.
 
-SENTRY_RATELIMITER = 'sentry.ratelimits.redis.RedisRateLimiter'
+SENTRY_RATELIMITER = "sentry.ratelimits.redis.RedisRateLimiter"
 
 ##################
 # Update Buffers #
@@ -192,7 +188,7 @@ SENTRY_RATELIMITER = 'sentry.ratelimits.redis.RedisRateLimiter'
 # numbers of the same events being sent to the API in a short amount of time.
 # (read: if you send any kind of real data to Sentry, you should enable buffers)
 
-SENTRY_BUFFER = 'sentry.buffer.redis.RedisBuffer'
+SENTRY_BUFFER = "sentry.buffer.redis.RedisBuffer"
 
 ##########
 # Quotas #
@@ -201,7 +197,7 @@ SENTRY_BUFFER = 'sentry.buffer.redis.RedisBuffer'
 # Quotas allow you to rate limit individual projects or the Sentry install as
 # a whole.
 
-SENTRY_QUOTAS = 'sentry.quotas.redis.RedisQuota'
+SENTRY_QUOTAS = "sentry.quotas.redis.RedisQuota"
 
 ########
 # TSDB #
@@ -210,7 +206,15 @@ SENTRY_QUOTAS = 'sentry.quotas.redis.RedisQuota'
 # The TSDB is used for building charts as well as making things like per-rate
 # alerts possible.
 
-SENTRY_TSDB = 'sentry.tsdb.redis.RedisTSDB'
+SENTRY_TSDB = "sentry.tsdb.redissnuba.RedisSnubaTSDB"
+
+#########
+# SNUBA #
+#########
+
+SENTRY_SEARCH = "sentry.search.snuba.EventsDatasetSnubaSearchBackend"
+SENTRY_SEARCH_OPTIONS = {}
+SENTRY_TAGSTORE_OPTIONS = {}
 
 ###########
 # Digests #
@@ -218,43 +222,63 @@ SENTRY_TSDB = 'sentry.tsdb.redis.RedisTSDB'
 
 # The digest backend powers notification summaries.
 
-SENTRY_DIGESTS = 'sentry.digests.backends.redis.RedisBackend'
-
-################
-# File storage #
-################
-
-# Uploaded media uses these `filestore` settings. The available
-# backends are either `filesystem` or `s3`.
-
-SENTRY_OPTIONS['filestore.backend'] = 'filesystem'
-SENTRY_OPTIONS['filestore.options'] = {
-    'location': env('SENTRY_FILESTORE_DIR'),
-}
+SENTRY_DIGESTS = "sentry.digests.backends.redis.RedisBackend"
 
 ##############
 # Web Server #
 ##############
 
-# If you're using a reverse SSL proxy, you should enable the X-Forwarded-Proto
-# header and set `SENTRY_USE_SSL=1`
-
-if env('SENTRY_USE_SSL', False):
-    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
-
-SENTRY_WEB_HOST = '0.0.0.0'
+SENTRY_WEB_HOST = "0.0.0.0"
 SENTRY_WEB_PORT = 9000
 SENTRY_WEB_OPTIONS = {
-    # 'workers': 3,  # the number of web workers
+    "http": "%s:%s" % (SENTRY_WEB_HOST, SENTRY_WEB_PORT),
+    "protocol": "uwsgi",
+    # This is needed to prevent https://git.io/fj7Lw
+    "uwsgi-socket": None,
+    # These ase for proper HTTP/1.1 support from uWSGI
+    # Without these it doesn't do keep-alives causing
+    # issues with Relay's direct requests.
+    "http-keepalive": True,
+    "http-auto-chunked": True,
+    "http-chunked-input": True,
+    # the number of web workers
+    'workers': 3,
+    # Turn off memory reporting
+    "memory-report": False,
+    # Some stuff so uwsgi will cycle workers sensibly
+    'max-requests': 100000,
+    'max-requests-delta': 500,
+    'max-worker-lifetime': 86400,
+    # Duplicate options from sentry default just so we don't get
+    # bit by sentry changing a default value that we depend on.
+    'thunder-lock': True,
+    'log-x-forwarded-for': False,
+    'buffer-size': 32768,
+    'limit-post': 209715200,
+    'disable-logging': True,
+    'reload-on-rss': 600,
+    'ignore-sigpipe': True,
+    'ignore-write-errors': True,
+    'disable-write-exception': True,
 }
+
+###########
+# SSL/TLS #
+###########
+
+# If you're using a reverse SSL proxy, you should enable the X-Forwarded-Proto
+# header and enable the settings below
+
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+# SOCIAL_AUTH_REDIRECT_IS_HTTPS = True
+
+# End of SSL/TLS settings
 
 ###############
 # Mail Server #
 ###############
-
 
 email = env('SENTRY_EMAIL_HOST') or (env('SMTP_PORT_25_TCP_ADDR') and 'smtp')
 if email:
@@ -263,8 +287,7 @@ if email:
     SENTRY_OPTIONS['mail.password'] = env('SENTRY_EMAIL_PASSWORD') or ''
     SENTRY_OPTIONS['mail.username'] = env('SENTRY_EMAIL_USER') or ''
     SENTRY_OPTIONS['mail.port'] = int(env('SENTRY_EMAIL_PORT') or 25)
-    SENTRY_OPTIONS['mail.use-tls'] = env('SENTRY_EMAIL_USE_TLS', False)
-    SENTRY_OPTIONS['mail.list-namespace'] = env('SENTRY_EMAIL_LIST_NAMESPACE') or 'localhost'
+    SENTRY_OPTIONS['mail.use-tls'] = Bool(env('SENTRY_EMAIL_USE_TLS', False))
 else:
     SENTRY_OPTIONS['mail.backend'] = 'dummy'
 
@@ -279,26 +302,19 @@ SENTRY_OPTIONS['mail.mailgun-api-key'] = env('SENTRY_MAILGUN_API_KEY') or ''
 if SENTRY_OPTIONS['mail.mailgun-api-key']:
     SENTRY_OPTIONS['mail.enable-replies'] = True
 else:
-    SENTRY_OPTIONS['mail.enable-replies'] = env('SENTRY_ENABLE_EMAIL_REPLIES', False)
+    SENTRY_OPTIONS['mail.enable-replies'] = Bool(
+        env('SENTRY_ENABLE_EMAIL_REPLIES', False))
 
 if SENTRY_OPTIONS['mail.enable-replies']:
     SENTRY_OPTIONS['mail.reply-hostname'] = env('SENTRY_SMTP_HOSTNAME') or ''
-
-#####################
-# SLACK INTEGRATION #
-#####################
-slack = env('SLACK_CLIENT_ID') and env('SLACK_CLIENT_SECRET')
-if slack:
-    SENTRY_OPTIONS['slack.client-id'] = env('SLACK_CLIENT_ID')
-    SENTRY_OPTIONS['slack.client-secret'] = env('SLACK_CLIENT_SECRET')
-    SENTRY_OPTIONS['slack.verification-token'] = env('SLACK_VERIFICATION_TOKEN') or ''
 
 # If this value ever becomes compromised, it's important to regenerate your
 # SENTRY_SECRET_KEY. Changing this value will result in all current sessions
 # being invalidated.
 secret_key = env('SENTRY_SECRET_KEY')
 if not secret_key:
-    raise Exception('Error: SENTRY_SECRET_KEY is undefined, run `generate-secret-key` and set to -e SENTRY_SECRET_KEY')
+    raise Exception(
+        'Error: SENTRY_SECRET_KEY is undefined, run `generate-secret-key` and set to -e SENTRY_SECRET_KEY')
 
 if 'SENTRY_RUNNING_UWSGI' not in os.environ and len(secret_key) < 32:
     print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
@@ -310,11 +326,77 @@ if 'SENTRY_RUNNING_UWSGI' not in os.environ and len(secret_key) < 32:
 
 SENTRY_OPTIONS['system.secret-key'] = secret_key
 
-if 'GITHUB_APP_ID' in os.environ:
-    GITHUB_EXTENDED_PERMISSIONS = ['repo']
-    GITHUB_APP_ID = env('GITHUB_APP_ID')
-    GITHUB_API_SECRET = env('GITHUB_API_SECRET')
+############
+# Features #
+############
 
-if 'BITBUCKET_CONSUMER_KEY' in os.environ:
-    BITBUCKET_CONSUMER_KEY = env('BITBUCKET_CONSUMER_KEY')
-    BITBUCKET_CONSUMER_SECRET = env('BITBUCKET_CONSUMER_SECRET')
+SENTRY_FEATURES["projects:sample-events"] = False
+SENTRY_FEATURES.update(
+    {
+        feature: True
+        for feature in (
+            "organizations:discover",
+            "organizations:events",
+            "organizations:global-views",
+            "organizations:integrations-issue-basic",
+            "organizations:integrations-issue-sync",
+            "organizations:invite-members",
+            "organizations:sso-basic",
+            "organizations:sso-rippling",
+            "organizations:sso-saml2",
+            "organizations:performance-view",
+            "projects:custom-inbound-filters",
+            "projects:data-forwarding",
+            "projects:discard-groups",
+            "projects:plugins",
+            "projects:rate-limits",
+            "projects:servicehooks",
+        )
+    }
+)
+
+######################
+# GitHub Integration #
+#####################
+
+# GITHUB_APP_ID = 'YOUR_GITHUB_APP_ID'
+# GITHUB_API_SECRET = 'YOUR_GITHUB_API_SECRET'
+# GITHUB_EXTENDED_PERMISSIONS = ['repo']
+
+#########################
+# Bitbucket Integration #
+########################
+
+# BITBUCKET_CONSUMER_KEY = 'YOUR_BITBUCKET_CONSUMER_KEY'
+# BITBUCKET_CONSUMER_SECRET = 'YOUR_BITBUCKET_CONSUMER_SECRET'
+
+#########
+# Relay #
+#########
+
+SENTRY_RELAY_WHITELIST_PK = []
+SENTRY_RELAY_OPEN_REGISTRATION = True
+
+#######################
+# OpenIDConnect Auth  #
+######################
+
+sentry_oidc = env('OIDC_CLIENT_ID') or False
+
+if sentry_oidc:
+    OIDC_CLIENT_ID = str(env('OIDC_CLIENT_ID'))
+    OIDC_CLIENT_SECRET = str(env('OIDC_CLIENT_SECRET'))
+    OIDC_SCOPE = str(env('OIDC_SCOPE'))
+    sentry_oidc_domain = env('OIDC_DOMAIN') or False
+
+    if sentry_oidc_domain:
+        OIDC_DOMAIN = str(env('OIDC_DOMAIN'))
+        
+        sentry_oidc_issuer = env('OIDC_ISSUER') or False
+        if sentry_oidc_issuer:
+            OIDC_ISSUER = str(env('OIDC_ISSUER'))
+    else:
+        OIDC_AUTHORIZATION_ENDPOINT = str(env('OIDC_AUTHORIZATION_ENDPOINT'))
+        OIDC_TOKEN_ENDPOINT = str(env('OIDC_TOKEN_ENDPOINT'))
+        OIDC_USERINFO_ENDPOINT = str(env('OIDC_USERINFO_ENDPOINT'))
+        OIDC_ISSUER = str(env('OIDC_ISSUER'))
